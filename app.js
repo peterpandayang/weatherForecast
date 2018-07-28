7aeabfa83b787d829f6b699f61b922e765cfe6bc
peterpandayang <byang14@dons.usfca.edu>
2018-07-28 10:06:30 -0700

    add route to map the forcast page and main page

--- app.js
+++ app.js
@@ -12,12 +12,16 @@ weatherApp.config(function($routeProvider){
         templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
     })
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
 });
 
 // SERVICES
 weatherApp.service('cityService', function(){
-   this.city = 'New York, NY'; 
-});
+   this.city = 'New York, US'; 
+});    
 
 // CONTROLLERS
 weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
@@ -28,6 +32,30 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
     });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
     $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast?appid=c712638bbc91ff13a0e583682e7acd6b", 
+        {
+            callback: "JSON_CALLBACK"
+        },
+        {
+            get: {method: "JSONP"}
+        });
+    
+    $scope.weatherResult = $scope.weatherAPI.get(
+        {
+            q:$scope.city, cnt:$scope.days
+        });
+    
+    $scope.convertToFahrenheit = function(degK){
+        return Math.round((1.8) * (degK - 273) + 32);
+    }
+    
+    $scope.convertToDate = function(dt){
+        return new Date(dt * 1000);
+    }
+        
 }]);
